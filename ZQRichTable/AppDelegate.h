6099225fb23cec66c04a477b87af63852ac01067
zzq <578259544@qq.com>
2018-07-23 10:51:39 +0800

    Initial Commit

---
+++ ZQRichTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZQRichTable
+//
+//  Created by zzq on 2018/7/23.
+//  Copyright © 2018年 zzq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZQRichTableTests/ZQRichTableTests.m
@@ -0,0 +1,39 @@
+//
+//  ZQRichTableTests.m
+//  ZQRichTableTests
+//
+//  Created by zzq on 2018/7/23.
+//  Copyright © 2018年 zzq. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZQRichTableTests : XCTestCase
+
+@end
+
+@implementation ZQRichTableTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
